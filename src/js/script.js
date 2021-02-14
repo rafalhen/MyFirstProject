1c1b02b21974492f70196c19db32eb08cdf77c23
rafalhen <rafalhen@users.noreply.github.com>
2021-02-14 19:21:26 +0100

    modals and links

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,8 @@ document.querySelector('.hamburger').addEventListener('click', function(e) {
     toggleMenu()
 });
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+//const canvas = document.getElementById('canvas');
+//const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 150, 100);
